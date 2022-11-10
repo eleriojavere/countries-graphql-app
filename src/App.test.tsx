c1917b3931996bda2c2fefe02bb40adb92e22de2
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-11-10 23:59:38 +0200

    Add tests for loading state and missing API key state

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, renderHook, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  console.error = jest.fn();
+});
+
+function useCountriesMock() {
+  return useQuery({ queryKey: ["useCountries"], queryFn: () => {} });
+}
+
+export const renderApp = (client: QueryClient) => {
+  return render(
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  );
+};
+
+test("Displays error when missing an API key", async () => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  renderApp(client);
+
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  );
+
+  const { result } = renderHook(() => useCountriesMock(), { wrapper });
+
+  await waitFor(() => {
+    return expect(result.current.isLoading).toBe(false);
+  });
+  const element = screen.getByText(/Problem fetching countries:/);
+
+  expect(element).toBeInTheDocument();
+});
+
+test("Displays loading bar when API is fetching data", async () => {
+  const client = new QueryClient();
+
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  );
+
+  renderApp(client);
+
+  const { result } = renderHook(() => useCountriesMock(), { wrapper });
+
+  await waitFor(() => {
+    return expect(result.current.isLoading).toBe(false);
+  });
+
+  const element = screen.getByText(/Loading.../);
+  expect(element).toBeInTheDocument();
 });
